943205258cf5f124c5dfd23c9fa6b8bc56dc5a99
kopeeno <apichat.kop@gmail.com>
2016-02-01 08:56:10 +0700

    add detail

--- src/main/java/com/foamtec/qa/web/MainController.java
+++ src/main/java/com/foamtec/qa/web/MainController.java
@@ -1,9 +1,11 @@
 package com.foamtec.qa.web;
 
+import com.foamtec.qa.domain.FaRequest;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -16,5 +18,12 @@ public class MainController {
     @RequestMapping(value = "/", produces = "text/html")
     public String saleRequest(Model uiModel) {
         return "foamtec-index";
+    }
+
+    @RequestMapping(value = "/engapprove/{id}", produces = "text/html")
+    public String engViewApprove(@PathVariable("id") Long id, Model uiModel) {
+        FaRequest faRequest = FaRequest.findFaRequest(id);
+        uiModel.addAttribute("farequest", faRequest);
+        return "main-eng-approve";
     }
 }
